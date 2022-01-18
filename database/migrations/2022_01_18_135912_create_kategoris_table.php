a145843195ce81611b13781bb96802a86c0e1b57
nando <muhammad.rezki.ananda@gmail.com>
2022-01-18 21:05:40 +0700

    Add table kategories

---
+++ database/migrations/2022_01_18_135912_create_kategoris_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKategorisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kategories', function (Blueprint $table) {
+            $table->string('kode_kategori');
+            $table->string('nama_kategori');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kategories');
+    }
+}
